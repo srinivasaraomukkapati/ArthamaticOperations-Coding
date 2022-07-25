c68ef579901f49d09ed5c8574473014d2e83d83b
srinivasaraomukkapati <nivas226@gmail.com>
2022-07-25 17:48:53 +0530

    Comments Added

--- Arthmatic_00.py
+++ Arthmatic_00.py
@@ -1,6 +1,6 @@
 a=10
 b=4
-print("Addition= ",a+b)
-print("Substract= ",a-b)
-print("Product= ", a*b)
-print("Modulus= ", a%b)
+print("Addition= ",a+b)		#Addition
+print("Substract= ",a-b)	#Substract
+print("Product= ", a*b)		#Multiplication
+print("Modulus= ", a%b)		#Modulus
